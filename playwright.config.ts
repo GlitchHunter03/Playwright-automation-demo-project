713e50f3b97e8e07752617f20ee3fcb729321214
Yash <yasass4ever@gmail.com>
2025-12-17 11:03:15 +0530

    browser updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,23 @@
-import { defineConfig } from '@playwright/test';
+import {defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     actionTimeout: 5000,
     baseURL: 'http://demowebshop.tricentis.com',
   },
   testDir: './Tests',
+
+  projects:[
+     {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    // {
+    //   name: 'Firefox',
+    //   use: { browserName: 'firefox' },
+    // },
+
+  ],
 });
